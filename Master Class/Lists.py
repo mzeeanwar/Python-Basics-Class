b3ba0977b614d68a869dd92add4b20917fe6d18d
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-09-05 02:50:00 +0500

    Add CRUD operations example with lists in Python

---
+++ Master Class/Lists.py
@@ -0,0 +1,16 @@
+# CRUD with Lists
+marks = [85, 90, 78]
+
+# Create
+marks.append(92)
+
+# Read
+print("Marks:", marks)
+
+# Update
+marks[1] = 95
+
+# Delete
+marks.remove(78)
+
+print("Updated Marks:", marks)
