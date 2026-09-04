96939574753b052eb45eb9128552190613c5b880
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-09-05 02:56:36 +0500

    Add functions for addition and greeting

---
+++ Master Class/Functions.py
@@ -0,0 +1,10 @@
+# Functions with parameters and return values
+
+def add(a, b):
+    return a + b
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(add(5, 3))          # 8
+print(greet("Shan"))      # Hello, Shan!
