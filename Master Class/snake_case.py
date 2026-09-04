97379afcf4be81ffabcdea5b7357ce6dc1ccd57f
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-09-05 02:12:57 +0500

    Add multiplication table implementation in snake_case.py

---
+++ Master Class/snake_case.py
@@ -0,0 +1,7 @@
+# Multiplication Table using loops
+
+num = int(input("Enter a number for multiplication table: "))
+
+print(f"\nMultiplication Table of {num}")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
