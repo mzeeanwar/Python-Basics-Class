0b59862d0f9649c95320d76755ba6fdd7ae852a8
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-09-06 15:35:56 +0500

    Update print statements to use new values

--- Modules.py
+++ Modules.py
@@ -1,5 +1,6 @@
 # Importing custom module
 import MyModule
 
-print(MyModule.say_hello("Shan"))
-print("Square of 4:", MyModule.square(4))
+print(MyModule.say_hello("Zeeshan"))
+print("Square of 6:", MyModule.square(6))
+print("Cube of 3:", MyModule.cube(3))
